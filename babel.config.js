cc89ab549145d05192809aa191afa2aa034f2e91
newly.dev <api@newly.dev>
2026-09-07 13:46:44 +0000

    Upload asset 175f6144-ab80-4917-b811-dee9bda37c55.png

--- babel.config.js
+++ babel.config.js
@@ -41,7 +41,6 @@ module.exports = function (api) {
       ],
       ...EDITABLE_COMPONENTS,
       "@babel/plugin-proposal-export-namespace-from",
-      "react-native-worklets/plugin", // react-native-worklets/plugin must be listed last!
     ],
   };
 };
